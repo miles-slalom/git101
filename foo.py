96c0730a5d66baf349136d679ec9953aef19c8d6
Miles Erickson <miles.erickson@slalom.com>
2022-04-18 10:23:46 -0700

    Revert "Update bar application to offer one random beverage instead of all beverages"
    This reverts commit 02d9d5e2f69e2a461edb842a0c4ead0a471c382e.

--- foo.py
+++ foo.py
@@ -1,5 +1,4 @@
-import random
 from bar import beverages
 
-beverage = random.choice(beverages)
-print(f"How about a nice pint of {beverage}")
+for beverage in beverages:
+    print(f"How about a nice pint of {beverage}")
